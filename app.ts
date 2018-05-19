1d66a7e176708846c8ad612ad32b2d3592ecd111
AlenaGolovaneva <golovaneva.93@mail.ru>
2018-05-19 16:57:25 +0300

    Add var with specific function type

--- app.ts
+++ app.ts
@@ -26,5 +26,21 @@ function add(first: number | string, second: number | string): number | string {
     return result;
 }
 
-el.innerHTML += add(5, 6);
+el.innerHTML += " " + add(5, 6);
 el.innerHTML += " " + add("4", "5");
+
+
+function sum (x: number, y: number): number {
+    return x + y;
+};
+function subtract (a: number, b: number): number {
+    return a - b;
+};
+ 
+let op: (x:number, y:number) => number;
+ 
+op = sum;
+console.log(op(2, 4));  // 6
+ 
+op = subtract;
+console.log(op(6, 4));  // 2
