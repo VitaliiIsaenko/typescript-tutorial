b411d3e28451696786f2a663e5994376e7528981
AlenaGolovaneva <golovaneva.93@mail.ru>
2018-05-19 17:38:51 +0300

    Add modifiers and getters/setters to classes fields

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ var User = /** @class */ (function () {
         this.name = _name;
         this.age = _age;
     }
+    Object.defineProperty(User.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        set: function (name) {
+            this._name = name;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(User.prototype, "age", {
+        get: function () {
+            return this._age;
+        },
+        set: function (age) {
+            this._age = age;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return User;
 }());
 var tom = new User("Tom", 29);
--- app.ts
+++ app.ts
@@ -1,12 +1,26 @@
 var el = this.document.getElementById("content");
 
 class User {
-    name: string;
-    age: number;
+    private _id: number;
+    private _name: string;
+    private _age: number;
     constructor(_name: string, _age: number) {
 
         this.name = _name;
         this.age = _age;
+    }
+    public set name(name: string) {
+        this._name = name;
+    }
+    public get name(): string {
+        return this._name;
+    }
+
+    public set age(age: number) {
+        this._age = age;
+    }
+    public get age(): number {
+        return this._age;
     }
 }
 var tom: User = new User("Tom", 29);
@@ -50,5 +64,5 @@ function mathOp(x: number, y: number, operation: (a: number, b: number) => numbe
     return result;
 }
 
-el.innerHTML += `<br/> ${mathOp(5,5,(first, second) => first * second)} `;
-el.innerHTML += ` ${mathOp(6,5, op)} `
+el.innerHTML += `<br/> ${mathOp(5, 5, (first, second) => first * second)} `;
+el.innerHTML += ` ${mathOp(6, 5, op)} `
